c2cb48f98aebd0a092f010c8419d161338fc5d74
KAW <kkawmadie@gmail.com>
2020-12-24 09:59:21 +0530

    access routing to methods

--- src/main/java/com/redhat/demo/MainVerticle.java
+++ src/main/java/com/redhat/demo/MainVerticle.java
@@ -2,21 +2,24 @@ package com.redhat.demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
        Router router=Router.router(vertx);
-       router.get("/api/v1/hello").handler(ctx ->{
-           ctx.request().response().end("Hello vertx world!");
-       });
-       router.get("/api/v1/hello/:name").handler(ctx -> {
-            String name=ctx.pathParam("name");
-            ctx.request().response().end(String.format("Hello %s!",name));
-           
-       });
-       vertx.createHttpServer().requestHandler(router).listen(8081);
+       router.get("/api/v1/hello").handler(this::helloVertex);
+       router.get("/api/v1/hello/:name").handler(this::helloName);
+       vertx.createHttpServer().requestHandler(router).listen(8080);
+    }
+
+    void helloVertex(RoutingContext ctx){
+        ctx.request().response().end("Hello vertx world!");
+    }
+    void helloName(RoutingContext ctx){
+        String name=ctx.pathParam("name");
+        ctx.request().response().end(String.format("Hello %s!",name));
     }
 
 }
